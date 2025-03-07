a819619e36f23db0225fbc6cce871ec6f1eaf56e
lukashrtan <lukas.hrtan.s@gyarab.cz>
2025-03-07 12:17:04 +0100

    idk

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('one', TemplateView.as_view(template_name='main/one.html')),
+    path('two', TemplateView.as_view(template_name='main/two.html')),
 ]
