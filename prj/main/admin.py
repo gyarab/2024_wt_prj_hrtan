bf797398dc39b4a4ca8ed304d993f02bb2cb3ac6
lukashrtan <lukas.hrtan.s@gyarab.cz>
2025-03-21 12:19:05 +0100

    tryit

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Server
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "region", "cpu", "cores", "ghz", "storage", "port", "traffic", "price"]
+
+admin.site.register(Server, ServerAdmin)
 
 # Register your models here.
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Server(models.Model):
+    title = models.CharField(max_length=300)
+    region = models.CharField(max_length=20)
+    cpu = models.CharField(max_length=20)
+    cores = models.IntegerField(null=True, blank=True)
+    ghz = models.IntegerField(null=True, blank=True)
+    storage = models.CharField(max_length=30)
+    port = models.IntegerField(null=True, blank=True)
+    traffic = models.IntegerField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f'{self.title} {self.region} {self.cpu} {self.cores} {self.ghz} {self.storage} {self.port} {self.traffic} {self.price}'
+
+
+
 # Create your models here.
