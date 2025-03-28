b0973b6e72b1c2d42cb289a5d8068a6c31c18081
lukashrtan <lukas.hrtan.s@gyarab.cz>
2025-03-28 12:18:14 +0100

    database

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import Server
+from .models import Server, Provider, Cart, User, ServerComment
 
 class ServerAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "region", "cpu", "cores", "ghz", "storage", "port", "traffic", "price"]
 
-admin.site.register(Server, ServerAdmin)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "url"]
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["id", "server_id:"]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class ServerCommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "text", "server_id", "user_id"]
+
+
 
 # Register your models here.
+admin.site.register(Server, ServerAdmin)
+admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(ServerComment, ServerCommentAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,19 +1,54 @@
 from django.db import models
 
 class Server(models.Model):
-    title = models.CharField(max_length=300)
-    region = models.CharField(max_length=20)
-    cpu = models.CharField(max_length=20)
-    cores = models.IntegerField(null=True, blank=True)
-    ghz = models.IntegerField(null=True, blank=True)
-    storage = models.CharField(max_length=30)
-    port = models.IntegerField(null=True, blank=True)
-    traffic = models.IntegerField(null=True, blank=True)
-    price = models.IntegerField(null=True, blank=True)
-
+    title = models.CharField(max_length=300, default="")
+    region = models.CharField(max_length=20, default="")
+    cpu = models.CharField(max_length=20, default="")
+    cores = models.IntegerField(null=True, blank=True, default=0)
+    ghz = models.IntegerField(null=True, blank=True, default=0)
+    storage = models.CharField(max_length=30, default="")
+    port = models.IntegerField(null=True, blank=True, default=0)
+    traffic = models.IntegerField(null=True, blank=True, default=0)
+    price = models.IntegerField(null=True, blank=True, default=0)
+    provider_id = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return f'{self.title} {self.region} {self.cpu} {self.cores} {self.ghz} {self.storage} {self.port} {self.traffic} {self.price}'
+    
+    
+class Provider(models.Model):
+    title = models.CharField(max_length=100, default="")
+    url = models.URLField(max_length=200, default="")
+
+    def __str__(self):
+        return f'{self.title} {self.url}'
+    
+
+class Cart(models.Model):
+    server_id = models.IntegerField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return f'{self.server_id}'
+    
+
+class User(models.Model):
+    name = models.CharField(max_length=50, default="")
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+class ServerComment(models.Model):
+    text = models.TextField(default="")
+    server_id = models.IntegerField(null=True, blank=True, default=0)
+    user_id = models.ImageField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return f'{self.text} {self.server_id} {self.user_id}'
+
+
+
+
 
 
 
