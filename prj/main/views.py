806d02f7740880bdc7165e2c01fe7bb889d32df5
lukashrtan <lukas.hrtan.s@gyarab.cz>
2025-04-25 12:15:13 +0200

    yay

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from main.models import Server
 
+def get_homepage(request):
+    context = {
+        "servers": Server.objects.all().order_by("title")
+    }
+    return render(request, "main/homepage.html", context)
 # Create your views here.
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from main.views import get_homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='main/base.html')),
-    path('homepage', TemplateView.as_view(template_name='main/homepage.html')),
+    path('', get_homepage),
     path('offer', TemplateView.as_view(template_name='main/offer.html')),
     path('providers', TemplateView.as_view(template_name='main/providers.html')),
     path('wishlist', TemplateView.as_view(template_name='main/wishlist.html')),
