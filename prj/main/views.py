8bfb93ce1eaeee67e6f07ca9bb7863027c721b0a
lukashrtan <lukas.hrtan.s@gyarab.cz>
2025-05-02 12:14:14 +0200

    kk

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from main.models import Server
 
+
+# Create your views here.
 def get_homepage(request):
+    servers = Server.objects.all().order_by("price")
+
+    # check if search parameter is in the request
+    if request.GET.get("search"):
+        servers = servers.filter(title__icontains=request.GET.get("search"))
+    
     context = {
-        "servers": Server.objects.all().order_by("title")
+        "servers": servers
     }
     return render(request, "main/homepage.html", context)
-# Create your views here.
+
